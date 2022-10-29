a419e93a5a07da53c622a3294a82f3d21054b7f0
olubisifolarin <hollubissy4real10@gmail.com>
2022-10-29 02:22:52 +0100

    final commit

--- src/Home.js
+++ src/Home.js
@@ -4,8 +4,8 @@ function Home(){
     return(
         <div className="home">
             <img src={"images/mypics.jpg"} alt="my_pics" />
-            <a href="https://twitter.com/olubisi_folarin" id="twi">olubisi_folarin</a>
-            
+            <a href="https://twitter.com/olubisi_folarin" id="twitter">olubisi_folarin</a>
+            <a href="https://app.slack.com/client/T042F7V19Q8/C041JU70S5U" id="slack">Oluwalonibisi</a>
             
         </div>
     )
--- src/Link.js
+++ src/Link.js
@@ -2,12 +2,12 @@
 function Link(){
     return(
         <div className="link">
-            <a href="https://twitter.com/olubisi_folarin" id="twitter">Twitter Link</a>
-            <a href="https://training.zuri.team/" id="btn_zuri">Zuri Team</a>
-            <a href="http://books.zuri.team" id="books">Zuri Book</a>
-            <a href="https://books.zuri.team/" id="book_python">Python Books</a>
-            <a href="https://background.zuri.team" id="pitch">Background Check for Coders</a>
-            <a href="https://books.zuri.team/design-rules" id="book_design">Design Books</a>
+            
+            <a href="https://training.zuri.team/"  id="btn_zuri">Zuri Team</a>
+            <a href="http://books.zuri.team" title="Get your books here at Zuri" id="books">Zuri Book</a>
+            <a href="https://books.zuri.team/" title="Become a Pythonista in just 2weeks.Get your Python boook here, u will love it"id="book_python">Python Books</a>
+            <a href="https://background.zuri.team" title="Enroll for you training here at Zuri, its free" id="pitch">Background Check for Coders</a>
+            <a href="https://books.zuri.team/design-rules" title="this is where you will find books about design and coding " id="book_design">Design Books</a>
         </div>
 
     )
